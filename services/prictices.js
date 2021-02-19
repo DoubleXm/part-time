c05d6c86677706301ff5569b595ae013001c21fb
shuqingXu <xu18516321273@qq.com>
2021-02-19 21:33:29 +0800

    新增首页搜索功能；更新postman配置文件

--- services/prictices.js
+++ services/prictices.js
@@ -1,41 +1,41 @@
-const Prictice = require("../models/prictices");
-const { Op } = require("sequelize");
-const cityList = require("../config/addr.json");
+const Prictice = require('../models/prictices');
+const { Op } = require('sequelize');
+const cityList = require('../config/addr.json');
 
 class PricticeService {
   pricticeClassify() {
     return {
       rows: [
-        "软件",
-        "客服",
-        "行政",
-        "运营",
-        "翻译",
-        "销售",
-        "人力",
-        "导购",
-        "传单",
-        "礼仪",
-        "仓管",
-        "物流",
-        "模特",
-        "音乐",
-        "咨询",
-        "美术",
-        "设计",
-        "培训",
-        "教育",
-        "法律",
-        "助理",
-        "其他",
-      ],
+        '软件',
+        '客服',
+        '行政',
+        '运营',
+        '翻译',
+        '销售',
+        '人力',
+        '导购',
+        '传单',
+        '礼仪',
+        '仓管',
+        '物流',
+        '模特',
+        '音乐',
+        '咨询',
+        '美术',
+        '设计',
+        '培训',
+        '教育',
+        '法律',
+        '助理',
+        '其他'
+      ]
     };
   }
   // 获取兼职地址分类列表
   async parttimeClassify(ctx) {
     const { city_name } = ctx.query;
 
-    if (!city_name) ctx.throw(412, "请输入城市名");
+    if (!city_name) ctx.throw(412, '请输入城市名');
     for (let province = 0; province < cityList.length; province++) {
       const citys = cityList[province].mallCityList;
       for (let city = 0; city < citys.length; city++) {
@@ -57,23 +57,13 @@ class PricticeService {
      * @param end_time 实习 结束时间筛选
      * @param area_name 兼职 区 筛选
      */
-    let {
-      limit,
-      offset,
-      sort,
-      order,
-      category,
-      salary,
-      begin_time,
-      end_time,
-      area_name,
-      admin
-    } = ctx.query;
+    let { limit, offset, sort, order, category, salary, begin_time, end_time, area_name, admin, post } = ctx.query;
     // 给定初始值
     !limit ? (limit = 10) : (limit = parseInt(limit));
     !offset ? (offset = 1) : (offset = parseInt(offset));
-    !sort ? (sort = "created_at") : sort;
-    !order ? (order = "DESC") : order;
+    !sort ? (sort = 'created_at') : sort;
+    !order ? (order = 'DESC') : order;
+    !post ? (post = '') : post;
     // where 对象构建
     let sql;
     const whereCondition = {};
@@ -82,8 +72,8 @@ class PricticeService {
         whereCondition[Op.or] = [{ category }];
       }
       if (salary && salary.trim()) {
-        const res = salary.split(",");
-        whereCondition.salary = { [Op.between]: res.map((v) => Number(v)) };
+        const res = salary.split(',');
+        whereCondition.salary = { [Op.between]: res.map(v => Number(v)) };
       }
       if (begin_time && end_time) {
         whereCondition.begin_time = { [Op.gte]: new Date(Number(begin_time)) };
@@ -94,34 +84,31 @@ class PricticeService {
       }
       // console.log({ ...whereCondition })
       sql = {
-        where: { ...whereCondition },
+        where: { ...whereCondition, post: { [Op.substring]: post } },
         limit,
         offset: (offset - 1) * limit,
-        order: [[sort, order]],
+        order: [[sort, order]]
       };
     } else {
-      sql = { limit, offset: (offset - 1) * limit, order: [[sort, order]] };
+      sql = { where: { post: { [Op.substring]: post } }, limit, offset: (offset - 1) * limit, order: [[sort, order]] };
     }
     // 如果传入了admin字段就代表是admin调用这个接口
     let total;
     if (admin) {
-      total = await Prictice.count({paranoid: false})
-      sql.paranoid = false
+      total = await Prictice.count({ paranoid: false });
+      sql.paranoid = false;
     } else {
       total = await Prictice.count();
     }
     const data = await Prictice.findAll(sql);
     return {
       total,
-      isNext:
-        total > limit * (offset - 1 === 0 ? 1 : offset - 1)
-          ? true
-          : false,
-      rows: [...data],
+      isNext: total > limit * (offset - 1 === 0 ? 1 : offset - 1) ? true : false,
+      rows: [...data]
     };
   }
 
-  // admin 
+  // admin
   async addAdminPrictice(ctx) {
     const {
       category,
@@ -133,7 +120,7 @@ class PricticeService {
       post,
       company_auth,
       cityName,
-      areaName,
+      areaName
     } = ctx.request.body;
     const data = await Prictice.create({
       category,
@@ -145,7 +132,7 @@ class PricticeService {
       post,
       company_auth,
       cityName,
-      areaName,
+      areaName
     });
     return { info: { ...data.toJSON() } };
   }
@@ -161,25 +148,28 @@ class PricticeService {
       post,
       company_auth,
       cityName,
-      areaName,
+      areaName
     } = ctx.request.body;
-    await Prictice.update({
-      category,
-      salary,
-      begin_time,
-      end_time,
-      shop_img,
-      shop_name,
-      post,
-      company_auth,
-      cityName,
-      areaName,
-    }, { where: { id } });
+    await Prictice.update(
+      {
+        category,
+        salary,
+        begin_time,
+        end_time,
+        shop_img,
+        shop_name,
+        post,
+        company_auth,
+        cityName,
+        areaName
+      },
+      { where: { id } }
+    );
     const data = await Prictice.findOne({ where: { id } });
     return { info: { ...data.toJSON() } };
   }
   async delAdminPrictice(ctx) {
-    const ids = ctx.params.id.split(",");
+    const ids = ctx.params.id.split(',');
     for (let i = 0; i < ids.length; i++) {
       await Prictice.destroy({ where: { id: ids[i] } });
     }
